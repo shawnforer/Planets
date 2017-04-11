886ae8ee1eacb3d4bff33dff0af257f9f4205647
Sebastien32 <sbw0302@gmail.com>
2017-04-10 22:31:59 -0700

    Create e7_planets_player.m

---
+++ e7_planets_player.m
@@ -0,0 +1,22 @@
+function [where] = e7_planets_player(map)
+location_scrap = map.scraps(1).location;
+location_player = map.player.location(end, :);
+scrap_row = location_scrap(1);
+scrap_column = location_scrap(2);
+player_row = location_player(1);
+player_column = location_player(2);
+if (scrap_row < player_row && ~isinf(map.grid(player_row - 1, player_column)))
+    where = 'U';
+    return
+elseif (scrap_row > player_row && ~isinf(map.grid(player_row + 1, player_column)))
+    where = 'D';
+    return
+elseif (scrap_column < player_column && ~isinf(map.grid(player_row, player_column - 1)))
+    where = 'L';
+    return
+elseif (scrap_column > player_column && ~isinf(map.grid(player_row, player_column + 1)))
+    where = 'R';
+    return
+end
+where = '.';
+end
