9495c92f2ea289b1c0ced7aadaaa9014ba25e62e
Sebastien32 <sbw0302@gmail.com>
2017-04-26 16:39:15 -0700

    Update map_to_array.m

--- map_to_array.m
+++ map_to_array.m
@@ -6,27 +6,20 @@ for i = 1 : columns * rows
     if (isinf(map.grid(i)))
         continue
     end
-    %Checks the cell directly below your current cell
-    if (~isinf(map.grid(mod(i + columns * rows, columns * rows) + 1)))
-        %If you are not in the bottom row
+    if (~isinf(map.grid((floor((i - 1) / rows)) * rows + mod(i, rows) + 1)))
         if(mod(i, rows) ~= 0)
-            array(i, i + 1) = 1;
-        %If you are in the bottom row
+            array(i, i + 1) = 1 + map.grid(i);
         else
-            array(i, i - rows + 1) = 1;
+            array(i, i - rows + 1) = 1 + map.grid(i);
         end
     end
-    %Checks the cell directly to th right of your current cell
     if (~isinf(map.grid(mod(i + rows - 1, columns * rows) + 1)))
-        %If you are not in far right
         if(i <= columns * rows - rows)
-            array(i, i + rows) = 1;
-        %If you are in far right 
+            array(i, i + rows) = 1 + map.grid(i);
         else
-            array(i, i - rows * (columns - 1)) = 1;
+            array(i, i - rows * (columns - 1)) = 1 + map.grid(i);
         end
     end
 end
-%Assume that if cell 1 is connected to 2, 2 is connected to 1
 array = array  + array';
 end
