c2c01ad9c04ee260ab28861078a6fac55e23c3ba
shawnforer <shawnf98@gmail.com>
2017-04-27 17:03:16 -0700

    Update cell_above.m

--- cell_above.m
+++ cell_above.m
@@ -1,3 +1,5 @@
 function [index_above] = cell_above(index, rows, columns)
+%This function takes the linear index of your current cell, and size of the map (rows, columns), 
+%and retuns the linear index of the cell directly above it. 
 index_above = ((floor((index-1)/rows))-floor((index-2)/rows))*rows + mod(index - 1, rows) + (floor((index - 1)/rows))*rows;
 end
