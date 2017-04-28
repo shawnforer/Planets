785f0e8700dc9698d2b957e115f2b5e33e44623f
shawnforer <shawnf98@gmail.com>
2017-04-27 17:04:02 -0700

    Update cell_below.m

--- cell_below.m
+++ cell_below.m
@@ -1,3 +1,5 @@
 function [index_below] = cell_below(index, rows, columns)
+%This function takes the linear index of your current cell, and size of the map (rows, columns), 
+%and retuns the linear index of the cell directly below it. 
 index_below = (floor((index-1)/rows)) * rows + mod(index, rows)+1;
 end
