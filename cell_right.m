1ab49dcc61fecd329f6b04336738a6b3549b330a
shawnforer <shawnf98@gmail.com>
2017-04-27 17:05:31 -0700

    Update cell_right.m

--- cell_right.m
+++ cell_right.m
@@ -1,3 +1,5 @@
 function [index_right] = cell_right(index, rows, columns)
+%This function takes the linear index of your current cell, and size of the map (rows, columns), 
+%and retuns the linear index of the cell directly to the right of the original. 
 index_right = (mod(index + rows - 1, columns * rows) + 1);
 end
